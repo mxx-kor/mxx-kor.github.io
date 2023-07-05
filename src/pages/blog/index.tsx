e88104c365b11427e68ecdf98e961ad96003495e
mxx-kor <minjae7728@gmail.com>
2023-07-05 17:50:09 +0900

    feat: 타입 오류 해결과 포스팅 제목과 날짜 표시

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,25 +1,46 @@
 import { getDataBase } from "@/apis/notion";
 import BaseLayout from "@/components/Layouts/BaseLayout";
 import SubLayout from "@/components/Layouts/SubLayout";
-import { QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
 import Link from "next/link";
 import { ReactElement } from "react";
 
+type PostInfo = {
+  created_time: Date;
+  properties: {
+    name: { id: string; title: { plain_text: string }[] };
+    tag: { id: string; multi_select: { name: string }[] };
+  };
+};
+
 export async function getStaticProps() {
-  const posts = await getDataBase();
+  const db = await getDataBase();
+  const posts = db.results.map(post => {
+    if ("properties" in post) {
+      const result = {
+        properties: post.properties,
+        created_time: post.created_time,
+      };
+      return result;
+    }
+  });
 
   return { props: { posts } };
 }
 
-const Blog = ({ posts }: { posts: QueryDatabaseResponse }) => {
+const Blog = ({ posts }: { posts: PostInfo[] }) => {
   return (
     <>
       <div className="text-3xl">Blog</div>
-      {posts.results.map((post, idx) => (
-        <Link key={idx} href="/blog/1">
-          {post.id}
-        </Link>
-      ))}
+      <ul>
+        {posts.map((post, idx) => (
+          <li key={idx}>
+            {JSON.stringify(post.created_time)}
+            <Link href="/blog/1">
+              {post.properties.name.title[0].plain_text}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
