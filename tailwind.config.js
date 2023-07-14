7a6350bcc9fa8052b92e552c44f34dcb60cc50dc
mxx-kor <minjae7728@gmail.com>
2023-07-14 23:59:46 +0900

    feat: tailwind color theme 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: "rgb(107 114 128)",
+        brown: "rgb(120 53 15)",
+        orange: "rgb(249 115 22)",
+        yellow: "rgb(253 224 71)",
+        green: "rgb(22 163 74)",
+        blue: "rgb(37 99 235)",
+        purple: "rgb(139 92 246)",
+        pink: "rgb(219 39 119)",
+        red: "rgb(220 38 38)",
+      },
+    },
   },
   plugins: [],
 };
