e2f7a78009dbee9305315f60dddcab415e3480b0
mxx-kor <minjae7728@gmail.com>
2023-09-05 03:47:34 +0900

    fix: Giscus repository 이름 변경 적용

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "export",
   env: {
-    COMMENTS_REPO: "mxx-kor/minjae.github.io",
+    COMMENTS_REPO: "mxx-kor/mxx-kor.github.io",
     COMMENTS_REPO_ID: "R_kgDOJ2XnQw",
     COMMENTS_CATEGORY: "Comments",
     COMMENTS_CATEGORY_ID: "DIC_kwDOJ2XnQ84CXxYD",
