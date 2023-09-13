aa42b644c0ee587308aacab1c5e1880dc8aa4cb7
mxx-kor <minjae7728@gmail.com>
2023-09-13 17:42:19 +0900

    chore: 효율적인 스타일링 작업 환경을 위한 패키지 설치
    설치한 패키지
    - prettier-plugin-tailwindcss (설치를 위해 prettier 3.0으로 버전업)
    - clsx
    - tailwind-merge

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  bracketSpacing: true,
+  endOfLine: "auto",
+  useTabs: false,
+  arrowParens: "avoid",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
