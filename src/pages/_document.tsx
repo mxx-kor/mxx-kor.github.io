4ad1eb97a5488f19e0c6d6142064f6710a55f510
mxx-kor <minjae7728@gmail.com>
2023-09-23 23:51:22 +0900

    feat: 구글 서치 콘솔 소유권 등록

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Head, Html, Main, NextScript } from "next/document";
 const MyDocument = () => {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="XJtOBpU5U3tModhdmzxqFas0xo0_saGhnWxPEAC-NHk"
+        />
+      </Head>
       <body className="text-primary bg-primary transition-[background]">
         <Main />
         <NextScript />
