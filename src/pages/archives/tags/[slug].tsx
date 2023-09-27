56cf4c7ccfee11ba0c73df59f67f04e213f6b6c9
mxx-kor <minjae7728@gmail.com>
2023-09-27 12:28:05 +0900

    feat: Tags 페이지 구현, 디자인

--- src/pages/archives/tags/[slug].tsx
+++ src/pages/archives/tags/[slug].tsx
@@ -1,7 +1,91 @@
-import React from "react";
+import { getDataBase } from "@/apis/notion";
+import PostListItem from "@/components/PostListItem";
+import { PageSEO } from "@/components/SEO";
+import Badge from "@/components/base/Badge";
+import Divider from "@/components/base/Divider";
+import PlainText from "@/components/base/PlainText";
+import SubTitle from "@/components/base/SubTitle";
+import Title from "@/components/base/Title";
+import { typeGuardedPosts } from "@/libs/notion";
+import { PostInfo } from "@/types/notion";
+import { GetStaticPropsContext } from "next";
+import { m } from "framer-motion";
+import { fadeInUp, staggerChild } from "@/libs/animations";
 
-const TagPage = () => {
-  return <div>TagPage</div>;
+export const getStaticPaths = async () => {
+  const db = await getDataBase();
+  const posts = typeGuardedPosts(db);
+  const allTags = Array.from(
+    posts.reduce((acc, post) => {
+      if ("Tags" in post.properties) {
+        if ("multi_select" in post.properties.Tags) {
+          post.properties.Tags.multi_select.forEach(tag => acc.add(tag.name));
+        }
+      }
+      return acc;
+    }, new Set<string>([])),
+  );
+  const paths = allTags.map(tag => ({
+    params: { slug: tag },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const slug = context.params && context.params.slug;
+  const db = await getDataBase({
+    filter: {
+      property: "Tags",
+      multi_select: {
+        contains: slug,
+      },
+    },
+  });
+  const posts = typeGuardedPosts(db);
+
+  return { props: { slug, posts } };
+};
+
+const TagPage = ({ slug, posts }: { slug: string; posts: PostInfo[] }) => {
+  return (
+    <>
+      <PageSEO
+        title={`Tags - ${slug} | minjae`}
+        description={`${slug} 태그에 관련된 블로그 포스트를 모아놨어요.`}
+        path={`archive/tags/${slug}`}
+      />
+      <m.main initial="initial" animate="animate">
+        <Title className="my-6 text-4xl font-bold">
+          Tags
+          <Badge className="text-secondary ml-4 rounded-lg text-2xl">
+            #{slug}
+          </Badge>
+        </Title>
+        <PlainText>
+          <span className="text-primary">{posts.length}개</span>의 포스트가
+          있습니다.
+        </PlainText>
+        <section>
+          <SubTitle className="mb-4">Posts</SubTitle>
+          <Divider className="mb-4" />
+          <m.ul
+            variants={staggerChild}
+            className="mb-12 grid w-full gap-8 lg:grid-cols-2 lg:gap-12"
+          >
+            {posts.map(post => (
+              <m.li variants={fadeInUp} key={post.id} className="group">
+                <PostListItem {...post} />
+              </m.li>
+            ))}
+          </m.ul>
+        </section>
+      </m.main>
+    </>
+  );
 };
 
 export default TagPage;
